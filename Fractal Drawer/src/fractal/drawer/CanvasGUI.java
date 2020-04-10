f3e9f11a59b7ed19543771862e9ac78317eb069d
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2020-04-09 22:55:31 -0700

    Started up the project
    Now I just need to figure out how to make a GUI in Java again

---
+++ Fractal Drawer/src/fractal/drawer/CanvasGUI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fractal.drawer;
+import java.awt.*;
+/**
+ *
+ * @author Owner
+ */
+public class CanvasGUI {
+    public String fractalType = "custom";
+    //add features to GUI to allow for zoom, color changing, and more
+    //constructor
+    public CanvasGUI(){
+        //create basic GUI with a few buttons and a big canvas for the fractal
+        //add button to save fractal later
+    }
+    
+    //class setters
+    //class getters
+    //other methods
+    
+    //toString returns current fractal type
+    public String toString() {
+        return fractalType;
+    }
+}
---
+++ Fractal Drawer/src/fractal/drawer/FractalDrawer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fractal.drawer;
+
+import images.APImage;
+import images.Pixel;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+/**
+ *
+ * @author Owner
+ */
+public class FractalDrawer {
+    public static APImage theImage = new APImage("placeholder.jpg");
+    
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Create CanvasGUI
+        //Take user input for desired fractal among list of fractals or just have user drag line to make fractal
+        
+    }
+    
+}
