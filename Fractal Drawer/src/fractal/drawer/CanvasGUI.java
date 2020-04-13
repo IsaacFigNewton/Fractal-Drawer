4f51fd940090eda89eb17d6fc99d0b70f4f82c63
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2020-04-12 21:53:33 -0700

    Added a GUI
    The GUI has 1 bug that my friends and I can't solve

--- Fractal Drawer/src/fractal/drawer/CanvasGUI.java
+++ Fractal Drawer/src/fractal/drawer/CanvasGUI.java
@@ -5,20 +5,72 @@
  */
 package fractal.drawer;
 import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
 /**
  *
  * @author Owner
  */
 public class CanvasGUI {
     public String fractalType = "custom";
+    //create the Canvas
+    private Canvas c = new Canvas();
+    //get the canvas's Graphics to draw with
+    private Graphics g = c.getGraphics();
     //add features to GUI to allow for zoom, color changing, and more
     //constructor
     public CanvasGUI(){
-        //create basic GUI with a few buttons and a big canvas for the fractal
-        //add button to save fractal later
+        //Creating the Frame
+        JFrame frame = new JFrame("Fractal Drawer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+
+        //Creating the MenuBar and save as button
+        JMenuBar mb = new JMenuBar();
+        JMenu save = new JMenu("Save as");
+        mb.add(save);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter desired fractal type");
+        JTextField textField = new JTextField(10); // accepts upto 10 characters
+        JButton enterType = new JButton("Enter");
+        JButton resetText = new JButton("Reset");
+        
+        // Components added to the bottom panel using Flow Layout
+        panel.add(label); 
+        panel.add(textField);
+        panel.add(enterType);
+        panel.add(resetText);
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, c);
+        frame.setVisible(true);
     }
     
     //class setters
+    public void stroke(Color theColor) {
+        g.setColor(theColor);
+    }
+    public void setBackground(Color theColor) {
+        c.setBackground(theColor);
+    }
+    public void line(int x1, int y1, int x2, int y2) {
+        g.drawLine(x1, y1, x2, y2);
+    }
+    public void circle(int x, int y, int r) {
+        Graphics2D g2 = (Graphics2D) g;
+        Shape circle = new Ellipse2D.Double(x, y, r, r);
+        g2.draw(circle);
+    }
+    public void rect(int x1, int y1, int x2, int y2) {
+        Graphics2D g2 = (Graphics2D) g;
+        Shape rect = new Rectangle(x1, y1, x2, y2);
+        g2.draw(rect);
+    }
+    
     //class getters
     //other methods
     
--- Fractal Drawer/src/fractal/drawer/FractalDrawer.java
+++ Fractal Drawer/src/fractal/drawer/FractalDrawer.java
@@ -5,25 +5,32 @@
  */
 package fractal.drawer;
 
-import images.APImage;
-import images.Pixel;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+//import images.APImage;
+//import images.Pixel;
+//import java.io.*;
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author Owner
  */
 public class FractalDrawer {
-    public static APImage theImage = new APImage("placeholder.jpg");
+//    public static APImage theImage = new APImage("placeholder.jpg");
     
     /**
      * @param args the command line arguments
      */
     
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Create CanvasGUI
+        //create the GUI and canvas
+        CanvasGUI GUI = new CanvasGUI();
+        
+        //set the canvas up
+        GUI.stroke(Color.BLACK);
+        GUI.setBackground(Color.WHITE);
+        
+        //draw on the canvas
+        GUI.line(100, 100, 400, 400);
         //Take user input for desired fractal among list of fractals or just have user drag line to make fractal
         
     }
